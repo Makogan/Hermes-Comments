a4001ca5c298fc21817e1c74982df90c99b5d7d0
Camilo Talero <camilotalero96@hotmail.com>
2018-04-17 21:28:25 -0600

    First Commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# Hermes Comments
+
+Hermes Comments is a simple visual studio extension that I created to help structure C and C++ code (and other languages using // and /**/ comments). It's purpose is to automate the creation of more visually complex comments to structure and document code.
+
+## Features
+
+Using shortcuts you can now turn a selected text into one of three
+possible formats, centered, left justified or subsection.
+
+For example if there is an image subfolder under your extension project workspace:
+
+\!\[feature X\]\(Assets/Left.gif\)
+
+> Tip: Many popular extensions utilize animations. This is an excellent way to show off your extension! We recommend short, focused animations that are easy to follow.
+
+## Requirements
+
+If you have any requirements or dependencies, add a section describing those and how to install and configure them.
+
+## Extension Settings
+
+Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
+
+For example:
+
+This extension contributes the following settings:
+
+* `myExtension.enable`: enable/disable this extension
+* `myExtension.thing`: set to `blah` to do something
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+-----------------------------------------------------------------------------------------------------------
+
+## Working with Markdown
+
+**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+
+### For more information
+
+* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,227 @@
+'use strict';
+
+import * as vscode from 'vscode';
+//import {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} from 'vscode';
+
+var maximum = 90;
+var sepChar = '#';
+var frameChar = '=';
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Hermes Comments has started');
+
+    let cFrame = vscode.commands.registerCommand('extension.makeCenteredSection', () => {
+
+        let success = makeCenteredFramedText();
+
+        if(!success)
+        {
+            vscode.window.showInformationMessage('No text selected');
+        }
+    });
+
+    let lFrame = vscode.commands.registerCommand('extension.makeLeftSection', () => {
+
+        let success = makeLeftFramedText();
+
+        if(!success)
+        {
+            vscode.window.showInformationMessage('No text selected');
+        }
+    });
+
+    let subs = vscode.commands.registerCommand('extension.makeSubSection', () => {
+
+        let success = makeSubsectionText();
+
+        if(!success)
+        {
+            vscode.window.showInformationMessage('Selection not valid. Subsections must be one line');
+        }
+    });
+
+    let separator = vscode.commands.registerCommand('extension.makeSeparatorLine', () => {
+        makeSeparatorLine();
+    });
+
+    context.subscriptions.push(cFrame);
+    context.subscriptions.push(lFrame);
+    context.subscriptions.push(subs);
+    context.subscriptions.push(separator);
+}
+
+
+export function deactivate() {
+}
+
+function makeSubsectionText()
+{
+    let editor = vscode.window.activeTextEditor;
+    let selection = editor.selection;
+    let currentLineString = editor.document.getText(selection);
+
+    let lines = currentLineString.split('\n');
+    if(lines.length>1 || lines.length===0)
+    {
+        return false;
+    }
+
+    let text = currentLineString.trim();
+
+    let title = '//──── ' + text + ' ';
+    let gap = title.length;
+    for(let index=0; index<(maximum - gap); index++)
+    {
+        title += '─';
+    }
+
+    editor.edit( textEditorEdit => {
+        textEditorEdit.replace(
+            editor.selection,
+            title
+        );
+    });
+
+    return true;
+}
+
+function makeSeparatorLine()
+{
+    let sep = '//';
+
+    for (let index =0; index < maximum-2; index++)
+    {
+        sep+=sepChar;
+    }
+
+    let editor = vscode.window.activeTextEditor;
+    let line = editor.selection.active.line;
+
+    editor.edit( textEditorEdit => {
+        textEditorEdit.replace(
+            editor.document.lineAt(line).range,
+            sep
+        );
+    });
+
+}
+
+function makeLeftFramedText()
+{
+    let editor = vscode.window.activeTextEditor;
+    let selection = editor.selection;
+    let currentLineString = editor.document.getText(selection);
+
+    if(currentLineString==='')
+    {
+        return false;
+    }
+
+    let framedText = makeFramingText('=');
+    framedText += lJustifyText(currentLineString);
+    framedText += makeFramingText('=');
+
+    vscode.window.activeTextEditor.edit( textEditorEdit => {
+        textEditorEdit.replace(
+            editor.selection,
+            framedText
+        );
+    });
+
+    return true;
+}
+
+function makeCenteredFramedText()
+{
+    let editor = vscode.window.activeTextEditor;
+    let selection = editor.selection;
+    let currentLineString = editor.document.getText(selection);
+
+    if(currentLineString==='')
+    {
+        return false;
+    }
+
+    let framedText = makeFramingText(frameChar);
+    framedText += alignText(currentLineString);
+    framedText += makeFramingText(frameChar);
+
+    vscode.window.activeTextEditor.edit( textEditorEdit => {
+        textEditorEdit.replace(
+            editor.selection,
+            framedText
+        );
+    });
+
+    return true;
+}
+
+function lJustifyText(text)
+{
+    let lines = text.split('\n');
+    let resulText = '';
+
+    for(let index=0; index<lines.length; index++)
+    {
+        let line = lines[index].trim();
+        let fill = ' ';
+
+        resulText += ' *'+fill+line;
+
+        fill = makeTrailingWS(maximum-resulText.length-2);
+
+        resulText+=fill+'*\n';
+    }
+    resulText = '/*' + makeTrailingWS(maximum-4) + '*\n'+resulText +
+        ' *' +makeTrailingWS(maximum-4) + '*/\n';
+    return resulText;
+}
+
+function alignText(text)
+{
+    let lines = text.split('\n');
+    let resulText = '';
+
+    for(let index=0; index<lines.length; index++)
+    {
+        let line = lines[index].trim();
+        let length = line.length;
+        let fill = makeTrailingWS((maximum-length)/2 - 2);
+
+        resulText += ' *'+fill+line;
+        if(length%2!==0)
+        {
+            fill = fill.substr(0,fill.length-1);
+        }
+        resulText+=fill+'*\n';
+    }
+    resulText = '/*' + makeTrailingWS(maximum-4) + '*\n'+resulText +
+        ' *' +makeTrailingWS(maximum-4) + '*/\n';
+    return resulText;
+}
+
+function makeTrailingWS(tLength)
+{
+    let trailing = '';
+    for(let index=0; index < tLength; index++)
+    {
+        trailing+=' ';
+    }
+
+    return trailing;
+}
+
+function makeFramingText(char)
+{
+    let line = '';
+    line += '//';
+    for (let index = 0; index < maximum-2; index++)
+    {
+        line+=char;
+    }
+    line+='\n';
+
+    return line;
+}
+
